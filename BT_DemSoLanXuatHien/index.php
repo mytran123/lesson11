72b5ed1176ff6d50602a927fd637d48ca9698343
mytran123 <mytran091196@gmai.com>
2021-10-21 10:26:38 +0700

    bài tập

---
+++ BT_DemSoLanXuatHien/index.php
@@ -0,0 +1,19 @@
+<?php
+function countNumber($numbers,$value) {
+    $count = 0;
+    for ($i = 0; $i < count($numbers); $i++) {
+        if ($numbers[$i] == $value) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+$numbers = [4, 2, 5, 7, 2, 0, 2, 8];
+
+//foreach ($numbers as $value) {
+//    echo $value . " ";
+//}
+$countNumber = countNumber($numbers, $value);
+echo "</br>";
+echo "Number of times the number is " . $countNumber;
