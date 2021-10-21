ef0bad673b8fac00354812368490fa60f71e32b7
mytran123 <mytran091196@gmai.com>
2021-10-21 12:12:11 +0700

    bài tập

--- BT_GameDoanSo/index.php
+++ BT_GameDoanSo/index.php
@@ -1,26 +1,25 @@
 <?php
-function binarySearch($numbers, $needle) {
-    $first = 0;
-    $last = count($numbers) - 1;
-    while ($first <= $last) {
-        $mid = (int)(($first + $last) / 2);
+session_start();
+include_once "Game.php";
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
-        if ($needle < $numbers[$mid]) {
-            $last = $mid - 1;
-        } elseif ($needle > $numbers[$mid]) {
-            $last = $mid + 1;
-        } else {
-            return true;
-        }
-    }
-    return false;
-}
-
-$numbers = range(1,100,5);
-
-$number = 82;
-if (binarySearch($numbers, $number)) {
-    echo "$number Found \n";
-} else {
-    echo "$number not Found \n";
-}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post">
+    <h1>Số của bạn là <?php echo $_SESSION["mid"]?></h1>
+    <input type="submit" name="start" value="Start">
+    <input type="submit" name="higher" value="Higher">
+    <input type="submit" name="lower" value="Lower">
+    <input type="submit" name="win" value="Win">
+</form>
+</body>
+</html>
